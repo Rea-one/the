221da31794f0e6ea99893f00b6a69953ea1a6cb7
Rea-one <rs17365282727@petalmail.com>
2025-05-21 11:42:48 +0800

    0

--- qol/filetree/filetree.go
+++ qol/filetree/filetree.go
@@ -1,11 +1,20 @@
 package filetree
 
-import "fyne.io/fyne/v2"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
 
 type FileTree struct {
-	The *fyne.Container
+	The      *fyne.Container
+	Origin   *Origin
+	Activate *widget.Button
 }
 
 func NewFileTree() *FileTree {
+	the := &FileTree{}
+	the.Origin = nil
+	the.Activate = widget.NewButton("启动文件树", func() {
 
+	})
 }
